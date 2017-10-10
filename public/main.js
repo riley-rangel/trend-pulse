949040b254d81f323fe326546982cf984b03b7b5
Riley Rangel <rrangel91@gmail.com>
2017-10-10 16:27:47 -0700

    Define a render function to visualize trending terms. #1

--- public/main.js
+++ public/main.js
@@ -1,3 +1,17 @@
+/* global createElement */
+
 fetch('/trending')
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(data => {
+    data.forEach(term => {
+      console.log(renderTrendTerm(term))
+    })
+  })
+  .catch(reject => console.error(reject))
+
+function renderTrendTerm(term) {
+  const $element = createElement('div', {'class': 'term-card', 'data-keyword': term}, [
+    createElement('h3', {}, [term])
+  ])
+  return $element
+}
