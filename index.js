faacb53efe9df811e89b289437accfb16bb7f471
Riley Rangel <rrangel91@gmail.com>
2017-10-10 15:11:26 -0700

    Define a GET handler to request data on Top 20 Trending searches. #1

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const path = require('path')
-const gst = require('google-search trends')
+const gst = require('google-search-trends')
 
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/trending', (req, res) => {
+  gst.result((error, response) => {
+    if (error) {
+      console.error(error)
+      res.sendStatus(404)
+      process.exit(1)
+    }
+    res.json(response['1'])
+  })
+})
 
 app.listen(3000, () => console.log('Port 3000 Open.'))
