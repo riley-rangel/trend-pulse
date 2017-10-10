47c26afe7a3f5c03050fcf7d1fe817eabc6e985c
Riley Rangel <rrangel91@gmail.com>
2017-10-10 16:39:13 -0700

    Define function to append rendered elements to home view. #1

--- public/main.js
+++ public/main.js
@@ -1,16 +1,21 @@
 /* global createElement */
 
+const $home = document.querySelector('.home')
+
 fetch('/trending')
   .then(response => response.json())
   .then(data => {
     data.forEach(term => {
-      console.log(renderTrendTerm(term))
+      $home.appendChild(renderTrendTerm(term))
     })
   })
   .catch(reject => console.error(reject))
 
 function renderTrendTerm(term) {
-  const $element = createElement('div', {'class': 'term-card', 'data-keyword': term}, [
+  const $element = createElement('div', {
+    'class': 'term-card col s12 m6 l3 xl3',
+    'data-keyword': term
+  }, [
     createElement('h3', {}, [term])
   ])
   return $element
