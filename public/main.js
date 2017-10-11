4ac104043517deef6803727671be5341f4979ecd
Riley Rangel <rrangel91@gmail.com>
2017-10-11 08:20:21 -0700

    Reformatted home page to make more visually balanced. #1

--- public/main.js
+++ public/main.js
@@ -1,23 +1,33 @@
 /* global createElement */
 
 const $home = document.querySelector('.home')
+const today = new Date(Date.now())
 
 fetch('/trending')
   .then(response => response.json())
   .then(data => {
+    const $heading = createElement('h4', {'class': 'heading center'}, [
+      'Trending Searches - ' + (today.getMonth() + 1) + '/' + today.getDate() +
+        '/' + today.getFullYear()
+    ])
+    $home.appendChild($heading)
+    let number = 1
     data.forEach(term => {
-      $home.appendChild(renderTrendTerm(term))
+      $home.appendChild(renderTrendTerm(term, number))
+      number++
     })
   })
   .catch(reject => console.error(reject))
 
-function renderTrendTerm(term) {
+function renderTrendTerm(term, number) {
   const $element = createElement('div', {
-    'class': 'col s12 m6 l3 xl3',
+    'class': 'col s8 offset-s2',
     'data-keyword': term
   }, [
     createElement('div', {'class': 'term-card card white'}, [
-      createElement('h4', {'class': 'term-content card-content center'}, [term])
+      createElement('h5', {
+        'class': 'term-content card-content'
+      }, [number + '. ' + term])
     ])
   ])
   return $element
