a3cc5c63990384dda82e661be0b50092c1a0a06d
Riley Rangel <rrangel91@gmail.com>
2017-10-11 19:28:02 -0700

    Move 'data-keyword' attribute to 'term-card' element. #2

--- public/main.js
+++ public/main.js
@@ -20,11 +20,11 @@ fetch('/trending')
   .catch(reject => console.error(reject))
 
 function renderTrendTerm(term, number) {
-  const $element = createElement('div', {
-    'class': 'col s8 offset-s2',
-    'data-keyword': term
-  }, [
-    createElement('div', {'class': 'term-card card white'}, [
+  const $element = createElement('div', {'class': 'col s8 offset-s2'}, [
+    createElement('div', {
+      'class': 'term-card card white',
+      'data-keyword': term
+    }, [
       createElement('h5', {
         'class': 'term-content card-content'
       }, [number + '. ' + term])
