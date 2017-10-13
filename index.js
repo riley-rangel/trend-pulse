25bd37d011f4709d34d5542827ff71d4d198e65d
Riley Rangel <rrangel91@gmail.com>
2017-10-12 22:30:50 -0700

    Define a GET handler to request and return data from Trends API on specified keyword. #2

--- index.js
+++ index.js
@@ -18,4 +18,40 @@ app.get('/trending', (req, res) => {
   })
 })
 
+app.get('/trending/:keyword', (req, res) => {
+  const keyword = req.params.keyword
+  const payload = []
+
+  googleTrends.interestOverTime({
+    keyword: keyword,
+    startTime: new Date(Date.now() - (24 * 60 * 60 * 1000))
+  })
+    .then(response => JSON.parse(response))
+    .then(parsed => parsed.default)
+    .then(data => {
+      payload.push(data)
+    })
+    .catch(reject => {
+      console.error(reject)
+      res.sendStatus(500)
+      process.exit(1)
+    })
+
+  googleTrends.interestByRegion({
+    keyword: keyword,
+    startTime: new Date(Date.now() - (24 * 60 * 60 * 1000))
+  })
+    .then(response => JSON.parse(response))
+    .then(parsed => parsed.default)
+    .then(data => {
+      payload.push(data)
+      return payload
+    })
+    .then(payload => res.json(payload))
+    .catch(reject => {
+      console.error(reject)
+      res.sendStatus(500)
+    })
+})
+
 app.listen(3000, () => console.log('Port 3000 Open.'))
