0a924139538e8690fc3360f7ad8677cf06d29f63
Riley Rangel <rrangel91@gmail.com>
2017-10-14 15:52:39 -0700

    Update function calls to use updated function name and correct argument data. #3

--- public/main.js
+++ public/main.js
@@ -6,34 +6,9 @@ const today = new Date(Date.now())
 fetch('/trending')
   .then(response => response.json())
   .then(data => {
-    const $heading = createElement('h4', {'class': 'heading center'}, [
-      'Trending Searches - ' + (today.getMonth() + 1) + '/' + today.getDate() +
-        '/' + today.getFullYear()
-    ])
-    $home.appendChild($heading)
-    let number = 1
-    data.forEach(term => {
-      $home.appendChild(renderTrendTerm(term, number))
-      number++
-    })
+    $home.appendChild(renderHomeTrends(data, 1))
   })
   .catch(reject => console.error(reject))
-
-function renderTrendTerm(term, number) {
-  const $element = createElement('div', {'class': 'col s8 offset-s2'}, [
-    createElement('div', {
-      'class': 'term-card card white',
-      'data-keyword': term
-    }, [
-      createElement('h5', {
-        'class': 'term-content card-content'
-      }, [number + '. ' + term])
-    ])
-  ])
-  return $element
-}
-
-console.log(renderHomeTrends)
 
 function renderHomeTrends(termList, startNumber) {
   let number = startNumber
