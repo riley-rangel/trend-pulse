45a2aad9b7f30c3103bbc4262edc15515067659d
Riley Rangel <rrangel91@gmail.com>
2017-10-14 15:42:07 -0700

    Create HashRouter class; define constructor properties, which take a set of 'view' elements. #3

--- public/hash-router.js
+++ public/hash-router.js
@@ -1,2 +1,10 @@
 /* eslint-disable no-unused-vars */
 /* global queryString */
+
+class HashRouter {
+  constructor($views) {
+    this.isListening = false
+    this.handlers = {}
+    this.$views = Array.from($views)
+  }
+}
