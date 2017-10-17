253c4630f365e85f2353cda3f81c4095a88ab674
Riley Rangel <rrangel91@gmail.com>
2017-10-17 14:34:18 -0700

    Create new client instance of Twitter API. #4

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const googleTrends = require('google-trends-api')
 const Twitter = require('twitter')
 require('dotenv').config()
 
-console.log(Twitter)
+const client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+})
+
+console.log(client)
 
 const app = express()
 
